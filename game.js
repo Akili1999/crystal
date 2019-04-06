676966f52e761d242362a0dfe57909262bb801c8
unknown <sluground@gmail.com>
2019-04-06 19:54:31 -0400

    finished

--- game.js
+++ game.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
-//crystals//
-   var crystalOne= Math.floor(Math.random() * 12) +1;
-    var crystalTwo= Math.floor(Math.random() * 12) +1;
-     var crystalThree= Math.floor(Math.random() * 12) +1;
-     var crystalFour= Math.floor(Math.random() * 12) +1;
 
+
+//crystals//
+var crystalOne= Math.floor(Math.random() * 12) +1;
+var crystalTwo= Math.floor(Math.random() * 12) +1;
+var crystalThree= Math.floor(Math.random() * 12) +1;
+var crystalFour= Math.floor(Math.random() * 12) +1;
+
+//page text//
     $("#theScore").text(totalScore);
     $("#wins").text(wins);
     $("#losses").text(losses);
     $("#random").text(random)
+
+
+
 //Scores//
 var wins= 0,
     losses= 0,
     totalScore= 0;
+
+
 //Round number//
     var random= Math.floor(Math.random() * 120) +19;
-//crystal properties//
+
+//crystal functions//
 function winner(){
     alert("You Win!");
     wins++;
@@ -35,6 +44,7 @@ function loser(){
     $("#random").text(random)
 }
 
+//crystal properties//
 $("#c1").on("click", function(one) {
     console.log(crystalOne);
     totalScore+=crystalOne;
@@ -88,12 +98,4 @@ $("#c4").on("click", function(four) {
     }
 })
 console.log(totalScore);
-
-
-// var html = "<p>This Round's Number: "+ number + "</p>" +
-// "<p>Wins: "+ wins + "</p>" +
-// "<p>Losses: "+ losses + "</p>" +
-// "<p>Your Total Score Is: "+ totalScore;
-
-// document.querySelector('#game').innerHTML = html;
 });

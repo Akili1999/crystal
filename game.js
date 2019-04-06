53c59f2da661aefe068c7b24e5a59a6f15ca7700
Akili Mcmackin <47020399+Akili1999@users.noreply.github.com>
2019-04-05 23:35:19 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,12 @@
+//crystals//
+var crystalOne, crystalFour, crystalThree, crystalTwo= Math.floor((Math.random() * 12) + 1)
+//Scores//
+var wins= 0,
+    losses= 0,
+    totalScore= 0;
+//Round number//
+    var number= Math.floor((Math.random() * 120) + 19);
+//crystal properties//
+$("#c1").on("click", function() {
+    alert("me");
+});
